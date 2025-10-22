62001f49de203a4581a4752dc8b13cbd713ece12
Usuario App Juridica <usuario@miappjuridica.com>
2025-10-21 21:53:36 -0700

    Fix: Arreglar correctamente next.config.ts con typescript ignoreBuildErrors

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: { ignoreBuildErrors: true }
 };
 
 export default nextConfig;
-
-typescript: { ignoreBuildErrors: true },
