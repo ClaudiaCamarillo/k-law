78cbb640cc476f135340fe78e990473be888144d
Usuario App Juridica <usuario@miappjuridica.com>
2025-10-23 15:08:03 -0700

    🖥️ Forzar vista de escritorio completa en móvil

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
+        <meta name="viewport" content="width=1024, user-scalable=yes" />
         <meta name="HandheldFriendly" content="true" />
         <meta name="MobileOptimized" content="320" />
       </head>
