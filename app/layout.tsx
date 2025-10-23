fdeff70001f197aeb675f3db0b9f8c5d2c194c9d
Usuario App Juridica <usuario@miappjuridica.com>
2025-10-23 14:54:19 -0700

    🔥 FORZAR adaptación móvil completa con CSS específico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
+import "./mobile-force.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({
